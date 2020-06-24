b9a61ab5605a6ce840acd6764ae19dc8482f7f35
AJ73 <ajith.dharwar@gmail.com>
2020-06-24 10:17:34 +0100

    Onemore change plus

--- queryprocessortest.py
+++ queryprocessortest.py
@@ -1,10 +1,10 @@
 from queryprocessor import process_query
 
-def test_knows_who_wrote_romeo_and_juliet():
-	assert process_query("Who wrote Romeo and Juliet") == "William Shakespeare"
+def test_knows_what_is_your_name():
+	assert process_query("what is your name") == "Ajith Dharwar"
 
 def test_is_case_insensitive():
-	assert process_query("Who wrote romeo and JULIET") == "William Shakespeare"
+	assert process_query("What is your NAME") == "Ajith Dharwar"
 
 def test_returns_empty_string_if_cannot_process_query():
 	assert process_query("xxxx") == ""
