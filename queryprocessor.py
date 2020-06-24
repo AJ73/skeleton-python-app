6bdbe919656d21789c2cae6b84492007a54207aa
AJ73 <ajith.dharwar@gmail.com>
2020-06-24 09:51:05 +0100

    My Name

--- queryprocessor.py
+++ queryprocessor.py
@@ -1,4 +1,4 @@
 def process_query(query):
-	if ("romeo and juliet" in query.lower()):
-		return "William Shakespeare had great twists in his plots"
+	if ("what is your name" in query.lower()):
+		return "Ajith Dharwar"
 	return ""
